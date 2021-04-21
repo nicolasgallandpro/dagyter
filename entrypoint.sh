e203515b4bf5b9d1b8e491900ffa42f18036fa06
nicolasgallandpro <nicolas.galland.pro@gmail.com>
2021-04-21 22:16:29 +0200

    modify streamlit example

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ dagit -h "0.0.0.0" -p "3000" -w "/opt/dagster/conf/workspace.yaml" &
 
 jupyter lab --port=8888 --no-browser --ip=0.0.0.0 --allow-root &
 
-streamlit run /workspace/streamlitbook.py &
+streamlit run /workspace/streamlitbook.py  &
 
 python /wrapper/run.py
 
